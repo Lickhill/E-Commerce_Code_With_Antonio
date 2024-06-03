6a2b9c99cd9ffbce3585951427cab31201d0f6be
Likhil N Maiya <134200383+Lickhill@users.noreply.github.com>
2024-06-03 17:50:31 +0000

    made some changes with the add to cart functionality and the billboard in the home page

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -7,18 +7,21 @@ import ProductList from "@/components/product-list";
 export const revalidate = 0;
 
 const HomePage = async () => {
-    const billboard = await getBillboard('df0e09f1-ec53-4f56-96b8-6414c467f7af');
-    const products = await getProducts({ isFeatured: true })
-    return (
-        <Container>
-            <div className="pb-10 space-y-10">
-                <Billboard data={billboard} />
-                <div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products} />
-                </div>
-            </div>
-        </Container>
-    )
-}
+	const billboard = await getBillboard(
+		"c6e8ffe6-8324-4dc7-815d-0a0208799a36"
+	);
+
+	const products = await getProducts({ isFeatured: true });
+	return (
+		<Container>
+			<div className="pb-10 space-y-10">
+				<Billboard data={billboard} />
+				<div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
+					<ProductList title="Featured Products" items={products} />
+				</div>
+			</div>
+		</Container>
+	);
+};
 
 export default HomePage;
